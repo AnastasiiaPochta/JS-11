baedf4e9a3232a7a919b0fcbe101c0e124f75361
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-03-05 20:26:09 +0300

    homework lesson 12

---
+++ script.js
@@ -0,0 +1,41 @@
+//assign: evaluation
+
+var a = 5;  
+var b, c;
+
+b = ((a) * (5)); //додала більше дужок
+console.log(b); //працює так само
+b = ((c) = (b/2)); //додала більше дужок
+console.log(b); //працює так само
+console.log(c); //працює так само
+
+//Number: age
+
+// const age = prompt("How old are you?");
+// const currentYear = 2023;
+// let yearOfBirth = (currentYear - age);
+// alert ("You probably was born in " + yearOfBirth);
+
+//Number: temperature
+
+// const celsius = prompt("Enter the temperature in Celsius");
+// let fahrenheit = celsius * (9/5) + 32;
+// alert ("The tempeature in Fahrenheit is " + fahrenheit);
+
+//Number: divide
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
