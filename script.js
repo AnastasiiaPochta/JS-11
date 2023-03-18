63a9f7ab09ceb875ab337199936543e6e3abe0a1
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-03-18 23:11:23 +0300

    lesson 15 homework

--- script.js
+++ script.js
@@ -1,214 +1,179 @@
-const hwReview = prompt(`Введіть назву завдання: Number: odd або String: lexics і тд...`);
+// Confirms
+
+// const arr = [confirm(`Ви згодні на обробку даних?`), confirm(`Вам виповнилося 18 років?`), confirm(`Так чи ні?`)];
 
 
-if (hwReview.includes(`Number`)) { // Завдання із назвою Number: odd
-    const num = Number(prompt("Введіть число"));
-    if (isNaN(num)) {
-        alert(`Помилка перетворення числа!`)
-    }
-    else {
-        if (num % 2 == 0) {
-            alert(`Число парне!`)
-        }
-        else {
-            alert(`Число непарне!`)
-        }
-    }
-}
+
+// Prompts
+
+// const arr = [];
+// arr[0] = prompt(`Введіть Ваше Ім'я`);
+// arr[1] = prompt(`Введіть Ім'я Вашого кота`);
 
 
-else if (hwReview.includes(`String`)) { // Завдання із назвою String: lexics
-    const str = prompt("Введіть ваш текст");
-    str.includes("блін")
-        ? alert("Присутні некоректні слова!")
-        : str.includes("бля")
-            ? alert("Присутні некоректні слова!")
-            : alert("Все ок!");
-}
+
+// Item access
+
+// const arr = ['Піони', 'Тюльпани', 'Троянди', 'Фіалки', 'Маки', 'Польові квіти'];
+// const index = prompt('Введіть індекс елемента в масиві, який Вам подати?');
+// if (index <= arr.length - 1 && index >= 0) {
+//     alert(arr[index])
+// } else {
+//     alert(`В массиві немає елемента з таким індексом`)
+// }
+
+// індекс "length" виводить довжину масива, якщо забрати умову яку я додала
 
 
-else if (hwReview.includes(`Boolean 1`)) { // Завдання із назвою Boolean 1
-    const eigtheenPlus = confirm('Вам вже виповнилося 18 років?');
-    const proceed = confirm('Ви надаєте згоду на обробку пресональних даних?');
-}
+
+// Item change
+
+// const arr = [];
+// const i = prompt(`Введіть номер індексу в масиві, куди бажаєте зробити запис`);
+// arr[i] = prompt(`Введіть значення для цього індексу.`);
+// console.log(arr);
 
 
-else if (hwReview.includes(`Boolean: if`)) { // Завдання із назвою Boolean: if
-    const eigtheenPlus = confirm('Вам вже виповнилося 18 років?');
-    if (eigtheenPlus) {
-        alert(`Вам можна продовжити перегляд сторінки`)
-        const proceed = confirm('Ви надаєте згоду на обробку пресональних даних?');
-        if (proceed) {
-            alert(`Дякую!`)
-        }
-        else {
-            alert(`Не дуже то і треба було!`)
-        }
-    }
-    else {
-        alert(`Нажаль тобі сюди не можна, малюк`)
-    }
-}
+
+// Multiply table
+
+// const arr = [
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 2, 3, 4],
+//     [0, 2, 4, 6, 8],
+//     [0, 3, 6, 9, 12],
+//     [0, 4, 8, 12, 16]
+// ];
 
 
-else if (hwReview.includes(`Comparison`)) { // Завдання із назвою Comparison: sizes
-    const userSize = prompt('Введіть Ваш розмір в міжнародному форматі: S, M, L, XL і тд');
-    if (userSize === 'XXS') {
-        alert('В США у Вас 8 розмір')
-    }
-    else if (userSize === 'XS') {
-        alert('В США у Вас 10 розмір')
-    }
-    else if (userSize === 'S') {
-        alert('В США у Вас 12 розмір')
-    }
-    else if (userSize === 'M') {
-        alert('В США у Вас 14 розмір')
-    }
-    else if (userSize === 'L') {
-        alert('В США у Вас 16 розмір')
-    }
-    else if (userSize === 'XL') {
-        alert('В США у Вас 18 розмір')
-    }
-    else if (userSize === 'XXL') {
-        alert('В США у Вас 20 розмір')
-    }
-    else if (userSize === 'XXXL') {
-        alert('В США у Вас 22 розмір')
-    }
-    else {
-        alert('Введіть корректно розімр: формат XXS, XS, S...')
-    }
-}
+
+// Multiply table slice
+
+// const arr = [
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 2, 3, 4],
+//     [0, 2, 4, 6, 8],
+//     [0, 3, 6, 9, 12],
+//     [0, 4, 8, 12, 16]
+// ];
+// const newArr = arr.slice(1);
+// for (let i = 0; i < newArr.length; i++) {
+//     newArr[i] = newArr[i].slice(1)
+// }
+// console.log(newArr);
 
 
-else if (hwReview.includes(`Ternary`)) { // Завдання із назвою Ternary
-    const isMale = confirm('Ви чоловік?');
-    isMale ? alert('Ви чоловік') : alert('Ви жінка');
-}
+
+// IndexOf Word
+
+// const str = prompt(`Введіть рядок із декількох слів`);
+// const arr = str.split(` `);
+// const word = prompt(`Яке слово Вам потірбне з цього рядка?`);
+// const index = arr.indexOf(word) + 1;
+// (arr.indexOf(word) >= 0) ?
+//     alert(`Слово '${word}' є ${index} за рахунком у Вашому рядку`) :
+//     alert('Такого слова не було в рядку(');
 
 
-else if (hwReview.includes(`Prompt`)) { // Завдання із назвою Prompt: or
-    const age = prompt("How old are you?") || alert("You didn't answer or cancelled, it's a mistake");
-    if (age) {
-        const currentYear = 2023;
-        let yearOfBirth = currentYear - age;
-        alert("You were probably born in " + yearOfBirth);
-    }
-}
+
+// Reverse
+
+// const arr = [];
+// for (let i = 1; i <= 5; i++) {
+//     arr.push(prompt(`Введіть елемент масиву`));
+// }
+// const newArr = [];
+// for (let i = 1; i <= 5; i++) {
+//     newArr.push(arr.pop());
+// }
+// console.log(newArr);
 
 
-else if (hwReview.includes(`Confirm: or`)) { // Завдання із назвою Confirm: or this days
-    const shop = confirm("Шопінг?") || alert("Ти бяка!");
-}
+
+// Reverse 2
+
+// const arr = [];
+// for (let i = 1; i <= 5; i++) {
+//     arr.push(prompt(`Введіть елемент масиву`));
+// }
+// const newArr = [];
+// for (let i = 1; i <= 5; i++) {
+//     newArr.push(arr.pop());
+// }
+// const newArr2 = [];
+// for (let i = 1; i <= 5; i++) {
+//     newArr2.unshift(newArr.shift());
+// }
+// console.log(newArr2);
 
 
-else if (hwReview.includes(`Confirm: if`)) { // Завдання із назвою Confirm: if this days
-    const shop = confirm("Шопінг?");
-    if (!shop) {
-        alert("Ти - бяка!");
-    }
-}
+
+// Copy
+
+// const arr = [
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 2, 3, 4],
+//     [0, 2, 4, 6, 8],
+//     [0, 3, 6, 9, 12],
+//     [0, 4, 8, 12, 16]
+// ];
+// const copyArr = arr.slice();
 
 
-else if (hwReview.includes(`Default: or`)) { // Завдання із назвою Default: or
-    const name = prompt('Твоє ім\'я?') || 'Іван';
-    const middleName = prompt('Твоє по-батькові?') || 'Іванович';
-    const lastName = prompt('Твоє прізвище?') || 'Іванов';
-    alert(`${name} ${middleName} ${lastName}`);
-}
+
+// Deep Copy
+
+// const arr = [
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 2, 3, 4],
+//     [0, 2, 4, 6, 8],
+//     [0, 3, 6, 9, 12],
+//     [0, 4, 8, 12, 16]
+// ];
+// const arrCopy = JSON.parse(JSON.stringify(arr));
 
 
-else if (hwReview.includes(`Default: if`)) { // Завдання із назвою Default: if
-    let name = prompt('Твоє ім\'я?');
-    if (!name) {
-        name = 'Іван';
-    }
-    let middleName = prompt('Твоє по-батькові?');
-    if (!middleName) {
-        middleName = 'Іванович';
-    }
-    let lastName = prompt('Твоє прізвище?');
-    if (!lastName) {
-        lastName = 'Іванов';
-    }
-    alert(`${name} ${middleName} ${lastName}`);
-}
+
+// Array Equals
+
+// Це неможливо зробити так як в JavaScript порівняння масивів за допомогою оператора === порівнює посилання на об'єкти, а не їхній вміст.
 
 
-else if (hwReview.includes(`Login`)) { // Завдання із назвою Login and password
-    let login = prompt("Введіть логін:");
-    if (login === "admin") {
-        let password = prompt("Введіть пароль:");
-        if (password === "qwerty") {
-            alert("Привіт, " + login + "!");
-        } else {
-            alert("Пароль невірний!");
-        }
-    } else {
-        alert("Логін невірний!");
-    }
-}
+
+//Flat
+
+// const arr = [
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 2, 3, 4],
+//     [0, 2, 4, 6, 8],
+//     [0, 3, 6, 9, 12],
+//     [0, 4, 8, 12, 16]
+// ];
+
+// const flatArr = [...arr[0], ...arr[1], ...arr[2], ...arr[3], ...arr[4]];
+// console.log(flatArr);
 
 
-else if (hwReview.includes(`Currency`)) { // Завдання із назвою Currency exchange
-    let currency = prompt("Введіть назву валюти (USD або EUR):").toUpperCase();
-    let isBuying = confirm("Ви хочете купити цю валюту?");
-    let rate;
-    if (currency === "USD") {
-        rate = isBuying ? 41.5 : 40.0;
-    } else if (currency === "EUR") {
-        rate = isBuying ? 43.0 : 41.5;
-    } else {
-        alert("Невідома валюта!");
-    }
-    if (rate) {
-        let amount = parseFloat(prompt("Введіть суму, яку ви хочете обміняти:"));
-        let result = isBuying ? amount / rate : amount * rate;
-        alert(`Ви отримаєте ${result.toFixed(2)} ${isBuying ? currency : "гривень"}.`);
-    }
-}
+
+//Destruct
+
+// const str = prompt(`Введіть якийсь рядок`);
+// const [a,,,,b,,,,c] = str;
+// alert (`Я витягнув '${a}', '${b}' та '${c}'`);
 
 
-else if (hwReview.includes(`Scissors`)) { // Завдання із назвою Scissors
-    const userChoice = prompt("Введіть камінь, ножиці або папір:");
-    const computerChoice = Math.random();
-    let computerChoiceString;
-    if (computerChoice < 0.34) {
-        computerChoiceString = "камінь";
-    } else if (computerChoice < 0.67) {
-        computerChoiceString = "ножиці";
-    } else {
-        computerChoiceString = "папір";
-    }
-    alert("Комп'ютер вибрав: " + computerChoiceString);
-    let result;
-    if (userChoice === computerChoiceString) {
-        result = "нічия";
-    } else if (userChoice === "камінь") {
-        if (computerChoiceString === "ножиці") {
-            result = "користувач";
-        } else {
-            result = "комп'ютер";
-        }
-    } else if (userChoice === "ножиці") {
-        if (computerChoiceString === "папір") {
-            result = "користувач";
-        } else {
-            result = "комп'ютер";
-        }
-    } else if (userChoice === "папір") {
-        if (computerChoiceString === "камінь") {
-            result = "користувач";
-        } else {
-            result = "комп'ютер";
-        }
-    }
-    alert("Переможець: " + result);
-}
+
+//Destruct default
+
+// const str = prompt(`Введіть якийсь рядок`);
+// const [,a=`!`,,b=`!`,c=`!`] = str;
+// alert (`Я витягнув '${a}', '${b}' та '${c}'`);
 
 
-else {
-    alert("Некоректна назва завдання!")
-}
+
+//Multiply table rest  
+
+
+
+
+
