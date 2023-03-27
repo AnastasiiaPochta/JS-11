7b8d64e4a0dd1d1d09f5e1ce482a972b2273903d
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-03-27 23:09:19 +0300

    lesson 17 homework

--- script.js
+++ script.js
@@ -1,279 +1,167 @@
-// Confirms
+//Temperature
 
-// const arr = [confirm(`Ви згодні на обробку даних?`), confirm(`Вам виповнилося 18 років?`), confirm(`Так чи ні?`)];
+// const celsius = prompt("Enter the temperature in Celsius");
 
+// const fahrenheitFunc = (cels) => cels * (9/5) + 32;
 
+// alert ("The tempeature in Fahrenheit is " + fahrenheitFunc(celsius));
+// console.log (fahrenheitFunc(15));
+// console.log (fahrenheitFunc(-5));
 
-// Prompts
+//блок коду функції для вирішення цього завдання не потрібний
 
-// const arr = [];
-// arr[0] = prompt(`Введіть Ваше Ім'я`);
-// arr[1] = prompt(`Введіть Ім'я Вашого кота`);
 
 
+//RGB
 
-// Item access
+// const rgb = (red, green, blue) =>
+//     ((red < 16) ? `0${(red).toString(16)}` : (red).toString(16)) +
+//     ((green < 16) ? `0${(green).toString(16)}` : (green).toString(16)) +
+//     ((blue < 16) ? `0${(blue).toString(16)}` : (blue).toString(16));
 
-// const arr = ['Піони', 'Тюльпани', 'Троянди', 'Фіалки', 'Маки', 'Польові квіти'];
-// const index = prompt('Введіть індекс елемента в масиві, який Вам подати?');
-// if (index <= arr.length - 1 && index >= 0) {
-//     alert(arr[index])
-// } else {
-//     alert(`В массиві немає елемента з таким індексом`)
-// }
+// let r = + prompt("Input your number for Red");
+// let g = + prompt("Input your number for Green");
+// let b = + prompt("Input your number for Blue");
 
-// індекс "length" виводить довжину масива, якщо забрати умову яку я додала
+// alert("Color: " + "#" + rgb(r, g, b))
 
+//блок коду функції для вирішення цього завдання не потрібний
 
 
-// Item change
 
-// const arr = [];
-// const i = prompt(`Введіть номер індексу в масиві, куди бажаєте зробити запис`);
-// arr[i] = prompt(`Введіть значення для цього індексу.`);
-// console.log(arr);
+//Flats
 
-
-
-// Multiply table
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-
-
-
-// Multiply table slice
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-// const newArr = arr.slice(1);
-// for (let i = 0; i < newArr.length; i++) {
-//     newArr[i] = newArr[i].slice(1)
-// }
-// console.log(newArr);
-
-
-
-// IndexOf Word
-
-// const str = prompt(`Введіть рядок із декількох слів`);
-// const arr = str.split(` `);
-// const word = prompt(`Яке слово Вам потірбне з цього рядка?`);
-// const index = arr.indexOf(word) + 1;
-// (arr.indexOf(word) >= 0) ?
-//     alert(`Слово '${word}' є ${index} за рахунком у Вашому рядку`) :
-//     alert('Такого слова не було в рядку(');
-
-
-
-// Reverse
-
-// const arr = [];
-// for (let i = 1; i <= 5; i++) {
-//     arr.push(prompt(`Введіть елемент масиву`));
-// }
-// const newArr = [];
-// for (let i = 1; i <= 5; i++) {
-//     newArr.push(arr.pop());
-// }
-// console.log(newArr);
-
-
-
-// Reverse 2
-
-// const arr = [];
-// for (let i = 1; i <= 5; i++) {
-//     arr.push(prompt(`Введіть елемент масиву`));
-// }
-// const newArr = [];
-// for (let i = 1; i <= 5; i++) {
-//     newArr.push(arr.pop());
-// }
-// const newArr2 = [];
-// for (let i = 1; i <= 5; i++) {
-//     newArr2.unshift(newArr.shift());
-// }
-// console.log(newArr2);
-
-
-
-// Copy
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-// const copyArr = arr.slice();
-
-
-
-// Deep Copy
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-// const arrCopy = JSON.parse(JSON.stringify(arr));
-
-
-
-// Array Equals
-
-// const arr1 = [1,2,3];
-// const arr2 = arr1;
-// arr2 === arr1; //true, оскільки це посилання на один і той же масив
-
-
-
-//Flat
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-// const flatArr = [...arr[0], ...arr[1], ...arr[2], ...arr[3], ...arr[4]];
-// console.log(flatArr);
-
-
-
-//Destruct
-
-// const str = prompt(`Введіть якийсь рядок`);
-// const [a,,,,b,,,,c] = str;
-// alert (`Я витягнув '${a}', '${b}' та '${c}'`);
-
-
-
-//Destruct default
-
-// const str = prompt(`Введіть якийсь рядок`);
-// const [,a=`!`,,b=`!`,c=`!`] = str;
-// alert (`Я витягнув '${a}', '${b}' та '${c}'`);
-
-
-
-//Multiply table rest  
-
-// const arr = [
-//     [0, 0, 0, 0, 0],
-//     [0, 1, 2, 3, 4],
-//     [0, 2, 4, 6, 8],
-//     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
-// ];
-// const [,[, ...rest1], [, ...rest2], [, ...rest3], [, ...rest4]] = arr;
-// const newArr = [rest1, rest2, rest3, rest4];
-// console.log(newArr);
-
-
-
-//For Alert
-
-// const names = ["John", "Paul", "George", "Ringo"];
-// for (const name of names) {
-//     alert(`${name}`)
-// }
-
-
-
-//For Select Option
-
-// const currencies = ["USD", "EUR", "GBP", "UAH"];
-// let  str = "<select>";
-// for (const currency of currencies){
-//     str += `<option>${currency}</option>`;
-// }
-// str+= "</select>";
-// document.write(str);
-
-
-
-//For Table Horizontal
-
-// const names = ["John", "Paul", "George", "Ringo"]
-// let str = "<table><tr>";
-// for (const name of names){
-//     str+= `<td>${name}</td>`
-// }
-// str+= "</tr></table>";
-// document.write(str);
-
-
-
-//For Table Vertical
-
-// const names = ["John", "Paul", "George", "Ringo"]
-// let str = "<table>";
-// for (const name of names){
-//     str+= `<tr><td>${name}</td></tr>`;
-// }
-// str+= "</table>";
-// document.write(str);
-
-
-
-//For Table Letters
-
-// const currencies = ["USD", "EUR", "GBP", "UAH"];
-// let str = "<table>";
-// for (const currency of currencies){
-//     str+= `<tr>`;
-//     for (const letter of currency){
-//         str+= `<td>${letter}</td>`;
+// const building = (floors, flats, flatNum) => {
+//     if (flatNum <= floors * flats) {
+//         const entranceNum = 1;
+//         const floorNum = Math.ceil(flatNum / flats);
+//         return {
+//             entrance: entranceNum,
+//             floor: floorNum
+//         }
 //     }
-//     str+= `</tr>`;
+//     else {
+//         const entranceNum = Math.ceil(flatNum / (floors * flats));
+//         const flatNumInEntrance = flatNum - (entranceNum - 1) * (floors * flats);
+//         const floorNum = Math.ceil(flatNumInEntrance / flats);
+//         return {
+//             entrance: entranceNum,
+//             floor: floorNum
+//         }
+//     }
 // }
-// str+= "</table>";
-// document.write(str);
+
+// const floor = prompt("How many floors does the building have?");
+// const flat = prompt("How many flats are there on the floor?");
+// const flatN = prompt("Which apartment do you need to find?");
+
+// console.log(building(floor, flat, flatN));
 
 
 
-//For Multiply Table
+//Credentials
 
-// const arr = [
+// const fullName = () => {
+//     const lastName = prompt("Введіть Ваше прізвище").trim();
+//     const name = prompt("Введіть Ваше ім'я").trim();
+//     const pobatkovi = prompt("Введіть По-батькові").trim();
+//     const surname = lastName.slice(0, 1).toUpperCase() + lastName.slice(1).toLowerCase();
+//     const correctName = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+//     const fatherName = pobatkovi.slice(0, 1).toUpperCase() + pobatkovi.slice(1).toLowerCase();
+//     return {
+//         correctName,
+//         surname,
+//         fatherName,
+//         fullName: `${surname} ${correctName} ${fatherName}`
+//     }
+// }
+// console.log(fullName());
+
+
+
+//New line
+
+// const newLine = (str) => str.split('\\n').join('\n');
+// const test = prompt("Введіть багаторядковий рядок. Ви можете вводити '\\n' як маркер нового рядка.");
+// console.log(newLine(test));
+
+
+
+//Prompt OR 
+
+// const promptOr = (prompt, mistake = "You didn't answer or cancelled, it's a mistake") =>
+// prompt ? alert(prompt) : alert(mistake);
+// const test = prompt(`How old are you?`);
+// promptOr(test);
+
+
+
+//Login And Password
+
+// const logPassCheck = (log, pas) => {
+//     const login = prompt("Введіть логін:");
+//     if (login === log) {
+//         let password = prompt("Введіть пароль:");
+//         if (password === pas) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     } else {
+//         return false;
+//     }
+// }
+// const trueLogin = "admin";
+// const truePassword = "qwerty";
+// alert(logPassCheck (trueLogin, truePassword));
+
+
+
+//For Table
+
+// const forTable = (arr) => {
+//     let str = "<table>";
+//     for (const number of arr) {
+//         str += `<tr>`;
+//         for (const num of number) {
+//             str += `<td>${num}</td>`;
+//         }
+//         str += `</tr>`;
+//     }
+//     str += "</table>";
+//     return document.write(str);
+// }
+
+// const test = [
 //     [0, 0, 0, 0, 0],
 //     [0, 1, 2, 3, 4],
 //     [0, 2, 4, 6, 8],
 //     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]
+//     [0, 4, 8, 12, 16]  
 // ];
-// let str = "<table>";
-// for (const number of arr){
-//     str+= `<tr>`;
-//     for (const num of number){
-//         str+= `<td>${num}</td>`;
-//     }
-//     str += `</tr>`;
+
+// const test2 = [
+//     [`Ivan`, `Petro`, `Anton`, `Stepan`, `Marichka`],
+//     [`Anastasiia`, `Elena`, `Ivan`, `Petro`, `Anton`],
+//     [`Anton`, `Stepan`, `Ilya`, `Alisa`, `Sergii`],
+//     [`Anastasiia`, `Elena`, `Ivan`, `Petro`, `Anton`],
+//     [`Ivan`, `Petro`, `Anton`, `Stepan`, `Marichka`]
+// ];
+
+// forTable(test);
+// forTable(test2);
+
+
+
+//Filter Lexics
+
+// const filterLexics = (str, arr) => {
+//     const strArr = str.toLowerCase().split(' ');
+//     const filter = strArr.map(word => arr.includes(word) ? '' : word);
+//     const str1 = filter.join(' ');
+//     return(str1);
 // }
-// str+= "</table>";
-// document.write(str);
+// const testStr = prompt(`Введіть рядок`);
+// const badWords = ['бляха', 'муха', "пляшка", "шабля"];
 
-
-
-//Function Capitalize
-
-// const capitalize = str => {
-//     let result = str.slice(0, 1).toUpperCase()+str.slice(1).toLowerCase();
-//     return result;
-// }
-// console.log(capitalize("cANBerRa"));
+// alert(filterLexics(testStr, badWords));
