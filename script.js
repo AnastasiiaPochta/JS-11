603fd240098c95729bbd75445cdd60fc08ba518a
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-03-29 17:38:41 +0300

    lesson 16 homework

--- script.js
+++ script.js
@@ -303,61 +303,61 @@
 
 //Table
 
-const car = [
-  {
-    Name: "chevrolet chevelle malibu",
-    Cylinders: 8,
-    Displacement: 307,
-    Horsepower: 130,
-    Weight_in_lbs: 3504,
-    Origin: "USA",
-  },
-  {
-    Name: "buick skylark 320",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 350,
-    Horsepower: 165,
-    Weight_in_lbs: 3693,
-    Acceleration: 11.5,
-    Year: "1970-01-01",
-  },
-  {
-    Miles_per_Gallon: 18,
-    Cylinders: 8,
-    Displacement: 318,
-    Horsepower: 150,
-    Weight_in_lbs: 3436,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "amc rebel sst",
-    Miles_per_Gallon: 16,
-    Cylinders: 8,
-    Displacement: 304,
-    Horsepower: 150,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-];
+// const car = [
+//   {
+//     Name: "chevrolet chevelle malibu",
+//     Cylinders: 8,
+//     Displacement: 307,
+//     Horsepower: 130,
+//     Weight_in_lbs: 3504,
+//     Origin: "USA",
+//   },
+//   {
+//     Name: "buick skylark 320",
+//     Miles_per_Gallon: 15,
+//     Cylinders: 8,
+//     Displacement: 350,
+//     Horsepower: 165,
+//     Weight_in_lbs: 3693,
+//     Acceleration: 11.5,
+//     Year: "1970-01-01",
+//   },
+//   {
+//     Miles_per_Gallon: 18,
+//     Cylinders: 8,
+//     Displacement: 318,
+//     Horsepower: 150,
+//     Weight_in_lbs: 3436,
+//     Year: "1970-01-01",
+//     Origin: "USA",
+//   },
+//   {
+//     Name: "amc rebel sst",
+//     Miles_per_Gallon: 16,
+//     Cylinders: 8,
+//     Displacement: 304,
+//     Horsepower: 150,
+//     Year: "1970-01-01",
+//     Origin: "USA",
+//   },
+// ];
 
-const columns = [];
-for (i = 0; i < car.length; i++) {
-  columns.push(...Object.keys(car[i]));
-}
-const uniqueColumns = [...new Set(columns)];
-let str = `<table><thead><tr>`;
-for (const column of uniqueColumns) {
-  str += `<th>${column}</th>`;
-}
-str += `</tr></thead><tbody>`;
-for (const row of car) {
-  str += "<tr>";
-  for (const column of uniqueColumns) {
-    str += `<td>${row[column] ? row[column] : ""}</td>`;
-  }
-  str += "</tr>";
-}
-str += "</tbody></table>";
-document.write(str);
+// const columns = [];
+// for (i = 0; i < car.length; i++) {
+//   columns.push(...Object.keys(car[i]));
+// }
+// const uniqueColumns = [...new Set(columns)];
+// let str = `<table><thead><tr>`;
+// for (const column of uniqueColumns) {
+//   str += `<th>${column}</th>`;
+// }
+// str += `</tr></thead><tbody>`;
+// for (const row of car) {
+//   str += "<tr>";
+//   for (const column of uniqueColumns) {
+//     str += `<td>${row[column] ? row[column] : ""}</td>`;
+//   }
+//   str += "</tr>";
+// }
+// str += "</tbody></table>";
+// document.write(str);
