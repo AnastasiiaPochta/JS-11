6cc17fab6ea65905a6737fe036d099b42fa33673
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-03-31 19:26:10 +0300

    lesson 17 homework

--- script.js
+++ script.js
@@ -87,7 +87,7 @@
 
 
 
-//Prompt OR 
+//Prompt OR
 
 // const promptOr = (prompt, mistake = "You didn't answer or cancelled, it's a mistake") =>
 // prompt ? alert(prompt) : alert(mistake);
@@ -120,24 +120,24 @@
 //For Table
 
 // const forTable = (arr) => {
-//     let str = "<table>";
-//     for (const number of arr) {
-//         str += `<tr>`;
-//         for (const num of number) {
-//             str += `<td>${num}</td>`;
-//         }
-//         str += `</tr>`;
+//   let str = "<table>";
+//   for (const number of arr) {
+//     str += `<tr>`;
+//     for (const num of number) {
+//       str += `<td>${num}</td>`;
 //     }
-//     str += "</table>";
-//     return document.write(str);
-// }
+//     str += `</tr>`;
+//   }
+//   str += "</table>";
+//   return document.write(str);
+// };
 
 // const test = [
 //     [0, 0, 0, 0, 0],
 //     [0, 1, 2, 3, 4],
 //     [0, 2, 4, 6, 8],
 //     [0, 3, 6, 9, 12],
-//     [0, 4, 8, 12, 16]  
+//     [0, 4, 8, 12, 16]
 // ];
 
 // const test2 = [
@@ -165,3 +165,98 @@
 // const badWords = ['бляха', 'муха', "пляшка", "шабля"];
 
 // alert(filterLexics(testStr, badWords));
+
+
+
+//Currency Table
+
+// const currTable = async () => {
+//   const arr = [];
+//   const res = await fetch("https://open.er-api.com/v6/latest/USD");
+//   const data = await res.json();
+//   const rates = data.rates;
+//   const currencies = Object.keys(rates);
+//   arr.push(JSON.parse(JSON.stringify(currencies)));
+//   arr[0].unshift(" ");
+//   for (const currency1 of currencies) {
+//     const row = [currency1];
+//     for (const currency2 of currencies) {
+//       if (currency1 === currency2) {
+//         row.push("1.00");
+//       } else {
+//         const crossRate = rates[currency2] / rates[currency1];
+//         const result = crossRate.toFixed(2);
+//         row.push(`${result}`);
+//       }
+//     }
+//     arr.push(row);
+//   }
+//   forTable(arr);
+// };
+
+// currTable();
+ 
+
+
+//Form
+
+// const form = (obj) => {
+//     let str = `<form><fieldset>`;
+//     for (const key in obj) {
+//         const type = typeof obj[key];
+//         if (type === "boolean") {
+//             const check = obj[key] ? "checked" : "";
+//             str += `<p><label> ${key}: <input type = "checkbox" ${check}/></label></p>`;
+//         } else if (type === "number") {
+//             str += `<p><label> ${key}: <input type = "${type}" value = "${obj[key]}"/></label></p>`;
+//         }
+//         else {
+//             str += `<p><label> ${key}: <input type = "text" value = "${obj[key]}"/></label></p>`;
+//         }
+//     }
+//     str += "</fieldset></form>";
+//     return document.write(str);
+// }
+
+// const car = {
+//     "Name": "chevrolet chevelle malibu",
+//     "Cylinders": 8,
+//     "Displacement": 307,
+//     "Horsepower": 130,
+//     "Weight_in_lbs": 3504,
+//     "Origin": "USA",
+//     "in_production": false
+// }
+
+// form(car);
+
+
+
+//Array of objects sort
+
+// const sort = (arr,key, boolean = true) => {
+//     let sortedArr = [...arr];
+//     if (typeof sortedArr[0][key] === 'number') {
+//         sortedArr.sort((a, b) => a[key] - b[key]);
+//     } else {
+//         sortedArr.sort((a, b) => {
+//             let x = a[key].toLowerCase();
+//             let y = b[key].toLowerCase();
+//             if (x < y) {return -1;}
+//             if (x > y) {return 1;}
+//             return 0;
+//         });
+//     }
+//     boolean ? sortedArr : sortedArr.reverse();
+//     return sortedArr;
+// }
+
+// const persons = [
+//     {name: "Іван", age: 17},
+//     {name: "Марія", age: 35},
+//     {name: "Олексій", age: 73},
+//     {name: "Яків", age: 12},
+// ]
+
+// console.log(sort(persons, "age"));
+// console.log(sort(persons, "name", false));
