7495c5461fd7479305375b7dc0b9557c6f83be54
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-04-06 12:29:08 +0300

    lesson 19 homework

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ let a = 10;
 }
 //a = 100, b - undefined, c - undefined, d - undefined
 
+
+
 //comparison if
 
 const age = +prompt("Скільки вам років?");
@@ -45,6 +47,8 @@ if (age < 0) {
 } else {
   alert("як пенсія?");
 }
+
+
 
 //switch: sizes
 
@@ -81,6 +85,8 @@ switch (userSize) {
     alert("Введіть корректно розімр: формат XXS, XS, S...");
 }
 
+
+
 //switch: if
 
 const color = prompt("Введіть колір");
@@ -103,4 +109,127 @@ if (color === "red") {
   document.write("<div style='background-color: gray;'>Я не зрозумів</div>");
 }
 
+
+
 //noswitch
+
+const noSwitch = (key, cases, defaultKey = "default") => {
+  if (key in cases) {
+    return cases[key]();
+  } else {
+    return cases[defaultKey]();
+  }
+};
+
+const drink = prompt("Що ви любите пити");
+noSwitch(drink, {
+  воду: () => console.log("Найздоровіший вибір!"),
+  чай() {
+    console.log("Смачна та корисна штука. Не перестарайтеся з цукром");
+  },
+  пиво: () => console.log("Добре влітку, та в міру"),
+  віскі: function () {
+    console.log("Та ви, батечку, естет! Не забудьте лід і сигару");
+  },
+  default() {
+    console.log("шото я не зрозумів");
+  },
+});
+
+
+
+//closure calc
+
+const buttonsDiv = document.getElementById("buttons");
+fetch("https://open.er-api.com/v6/latest/USD")
+  .then((res) => res.json())
+  .then((data) => {
+    const rates = data.rates;
+    const currencies = Object.keys(rates);
+    for (const currency of currencies) {
+      const btn = document.createElement("button");
+      btn.innerText = currency;
+      btn.onclick = () => {
+        const sum = prompt(
+          `Яку суму ${currency} ви хочете конвертувати в USD?`
+        );
+        const convert = (sum / rates[currency]).toFixed(2);
+        return alert(`Ви отримаєте ${convert} USDT`);
+      };
+      buttonsDiv.appendChild(btn);
+    }
+  });
+
+
+
+//closure calc 2
+
+const fromSelect = document.getElementById("from");
+const toSelect = document.getElementById("to");
+const amount = document.getElementById("amount");
+const resultDiv = document.getElementById("result");
+const rateDiv = document.getElementById("rate");
+
+fetch("https://open.er-api.com/v6/latest/USD")
+  .then((res) => res.json())
+  .then((data) => {
+    const rates = data.rates;
+    const currencies = Object.keys(rates);
+    for (const currency of currencies) {
+      const optn = document.createElement("option");
+      optn.innerText = currency;
+      fromSelect.appendChild(optn);
+    }
+    for (const currency of currencies) {
+      const optn = document.createElement("option");
+      optn.innerText = currency;
+      toSelect.appendChild(optn);
+    }
+    const convertCurrency = () => {
+      const cur1 = fromSelect.value;
+      const cur2 = toSelect.value;
+      const crossRate = rates[cur2] / rates[cur1];
+      const result = crossRate * amount.value;
+      rateDiv.innerText = crossRate;
+      resultDiv.innerText = result;
+    };
+    fromSelect.onchange = convertCurrency;
+    toSelect.onchange = convertCurrency;
+    amount.oninput = convertCurrency;
+  });
+
+
+  
+//countries and cities
+
+fetch(
+  "https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.min.json"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const countries = Object.keys(data);
+    const countriesSelect = document.createElement("select");
+    const citiesSelect = document.createElement("select");
+    const city = document.createElement("option");
+    city.innerText = " ";
+    citiesSelect.appendChild(city);
+    for (const country of countries) {
+      const optn = document.createElement("option");
+      optn.innerText = country;
+      countriesSelect.appendChild(optn);
+    }
+    document.body.appendChild(countriesSelect);
+    document.body.appendChild(citiesSelect);
+    const cityFunc = () => {
+      citiesSelect.innerHTML = "";
+      const selected = countriesSelect.value;
+      const cities = data[selected];
+      for (const city of cities) {
+        const optn = document.createElement("option");
+        optn.innerText = city;
+        citiesSelect.appendChild(optn);
+      }
+    };
+    countriesSelect.onchange = cityFunc;
+  });
