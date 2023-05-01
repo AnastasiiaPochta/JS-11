4d9da4561143b44a8835bbae1f1d2083d64f7f84
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-05-01 15:20:26 +0300

    redux hw

--- script.js
+++ script.js
@@ -12,40 +12,44 @@ function reducer(state, { type, what, quantity, money }) {
   if (type === "КУПИТИ") {
     if (quantity <= state[what].q) {
       if (money >= quantity * state[what].price) {
+        alert(`Ви придбали ${quantity} ${what} за ${quantity * state[what].price} грн. Ваша здача: ${money - quantity * state[what].price} грн`)
         return {
           ...state,
           [what]: { ...state[what], q: state[what].q - quantity },
           Casa: money - quantity * state[what].price
         };
+      } else {
+        alert('Вибачте, ви внесли недостатньо коштів!');
       }
+    } else {
+      alert('Вибачте, товар поки що відсутній');
     }
     return state;
   }
 }
 
 function createStore(reducer) {
-  let state = reducer(undefined, {}); //стартова ініціалізація стану, запуск редьюсера зі state === undefined
-  let cbs = []; //масив пiдписникiв
+  let state = reducer(undefined, {});
+  let cbs = [];
 
-  const getState = () => state; //функція, що повертає змінну із замикання
+  const getState = () => state;
   const subscribe = (cb) => (
-    cbs.push(cb), //запам'ятовуємо пiдписника у масиві
+    cbs.push(cb),
     () => (cbs = cbs.filter((c) => c !== cb))
-  ); //повертаємо функцію unsubscribe, яка видаляє пiдписника зі списку
+  );
 
   const dispatch = (action) => {
-    const newState = reducer(state, action); //пробуємо запустити редьюсер
+    const newState = reducer(state, action);
     if (newState !== state) {
-      //перевіряємо, чи зміг ред'юсер обробити action
-      state = newState; //якщо зміг, то оновлюємо state
-      for (let cb of cbs) cb(); //та запускаємо пiдписникiв
+      state = newState;
+      for (let cb of cbs) cb();
     }
   };
 
   return {
-    getState, //додавання функції getState в результуючий об'єкт
+    getState,
     dispatch,
-    subscribe, //додавання subscribe в об'єкт
+    subscribe,
   };
 }
 
@@ -80,7 +84,19 @@ const actionCreator = (what, quantity, money) => ({
 
 but.onclick = () => {
   store.dispatch(actionCreator(what, quantity, money));
-  console.log(store.getState())
+  inputM.value = '';
+  select.value = '';
+  inputQ.value = '';
 }
 
+const qBeer= document.getElementById("q_beer");
+const qChips= document.getElementById("q_chips");
+const qCigaretts= document.getElementById("q_cigaretts");
 
+const quantRenew = () => {
+  qBeer.innerText = `${store.getState().Beer.q}`;
+  qChips.innerText = `${store.getState().Chips.q}`;
+  qCigaretts.innerText = `${store.getState().Cigaretts.q}`;
+}
+store.subscribe(quantRenew);
+quantRenew();
