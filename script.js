f07ff9d8dd64a9f7dcc84f27d45bf0a43f798d50
Anastasiia Pochta <123012957+AnastasiiaPochta@users.noreply.github.com>
2023-05-23 20:00:09 +0300

    module

--- script.js
+++ script.js
@@ -27,211 +27,308 @@ function createStore(reducer) {
   };
 }
 
+//promiseReducer
 
+function promiseReducer(state = {}, { type, status, payload, error, key }) {
+  if (type === "PROMISE") {
+    return {
+      ...state,
+      [key]: { status, payload, error },
+    };
+  }
+  return state;
+}
 
-// //promiseReducer
+const actionPending = (key) => ({ type: "PROMISE", status: "PENDING", key });
 
-// function promiseReducer(state = {}, { type, status, payload, error, key}) {
-//   if (type === "PROMISE") {
-//     return {
-//       ...state,
-//       [key]: { status, payload, error },
-//     };
-//   }
-//   return state;
-// }
+const actionFulfilled = (key, payload) => ({
+  type: "PROMISE",
+  status: "FULFILLED",
+  payload,
+  key,
+});
 
-// const actionPending = (key) => ({ type: "PROMISE", status: "PENDING", key });
+const actionRejected = (key, error) => ({
+  type: "PROMISE",
+  status: "REJECTED",
+  error,
+  key,
+});
 
-// const actionFulfilled = (key, payload) => ({
-//   type: "PROMISE",
-//   status: "FULFILLED",
-//   payload,
-//   key,
-// });
+const actionPromise = (key, promise) => async (dispatch) => {
+  dispatch(actionPending(key));
+  try {
+    const payload = await promise;
+    dispatch(actionFulfilled(key, payload));
+    return payload;
+  } catch (error) {
+    dispatch(actionRejected(key, error));
+  }
+};
 
-// const actionRejected = (key, error) => ({
-//   type: "PROMISE",
-//   status: "REJECTED",
-//   error,
-//   key,
-// });
+const store = createStore(promiseReducer);
 
-// const delay = (ms) => new Promise((ok) => setTimeout(() => ok(ms), ms));
+store.subscribe(() => console.log(store.getState()));
 
-// const actionPromise = (key, promise) => async (dispatch) => {
-//   dispatch(actionPending(key));
-//   try {
-//     const payload = await promise;
-//     dispatch(actionFulfilled(key, payload));
-//     return payload;
-//   } catch (error) {
-//     dispatch(actionRejected(key, error));
-//   }
-// };
+//authReducer
 
-// const store = createStore(promiseReducer);
+const jwtDecode = (token) => {
+  try {
+    const arr = token.split(".");
+    const data = arr[1];
+    const decodedData = JSON.parse(atob(data));
+    return decodedData;
+  } catch (e) {
+    return "undefined";
+  }
+};
 
-// store.subscribe(() => console.log(store.getState())); //має запускатися 6 разів
+function authReducer(state = {}, { type, token }) {
+  if (type === "AUTH_LOGIN") {
+    const payload = jwtDecode(token);
+    return {
+      token,
+      payload,
+    };
+  } else if (type === "AUTH_LOGOUT") {
+    return {};
+  } else {
+    return state;
+  }
+}
 
-// store.dispatch(actionPromise("delay", delay(1000)));
+const actionAuthLogin = (token) => ({ type: "AUTH_LOGIN", token });
+const actionAuthLogout = () => ({ type: "AUTH_LOGOUT" });
 
-// store.dispatch(
-//   actionPromise(
-//     "luke",
-//     fetch("https://swapi.dev/api/people/1").then((res) => res.json())
-//   )
-// );
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOiI2Mzc3ZTEzM2I3NGUxZjVmMmVjMWMxMjUiLCJsb2dpbiI6InRlc3Q1IiwiYWNsIjpbIjYzNzdlMTMzYjc0ZTFmNWYyZWMxYzEyNSIsInVzZXIiXX0sImlhdCI6MTY2ODgxMjQ1OH0.t1eQlRwkcP7v9JxUPMo3dcGKprH-uy8ujukNI7xE3A0";
 
-// store.dispatch(
-//   actionPromise(
-//     "tatooine",
-//     fetch("https://swapi.dev/api/planets/1").then((res) => res.json())
-//   )
-// );
+const store2 = createStore(authReducer);
+store2.subscribe(() => console.log(store2.getState()));
 
+store2.dispatch(actionAuthLogin(token));
+store2.dispatch(actionAuthLogout());
 
+//cartReducer
 
-// //authReducer
+function cartReducer(state = {}, { type, count, good }) {
+  if (type === "CART_ADD") {
+    const { _id } = good;
+    if (state[_id]) {
+      return {
+        ...state,
+        [_id]: { ...state[_id], count: state[_id].count + count },
+      };
+    } else {
+      return {
+        ...state,
+        [_id]: { good, count },
+      };
+    }
+  } else if (type === "CART_SUB") {
+    const { _id } = good;
+    let newCount = state[_id].count - count;
+    if (newCount <= 0) {
+      const newState = { ...state };
+      delete newState[_id];
+      return newState;
+    } else {
+      return {
+        ...state,
+        [_id]: { ...state[_id], count: newCount },
+      };
+    }
+  } else if (type === "CART_DEL") {
+    const { _id } = good;
+    const newState = { ...state };
+    delete newState[_id];
+    return newState;
+  } else if (type === "CART_SET") {
+    const { _id } = good;
+    if (count <= 0) {
+      const newState = { ...state };
+      delete newState[_id];
+      return newState;
+    } else {
+      if (state[_id]) {
+        return {
+          ...state,
+          [_id]: { ...state[_id], count },
+        };
+      } else {
+        return {
+          ...state,
+          [_id]: { good, count },
+        };
+      }
+    }
+  } else if (type === "CART_CLEAR") {
+    return {};
+  } else {
+    return state;
+  }
+}
 
-// const jwtDecode = (token) => {
-//   try {
-//     const arr = token.split(".");
-//     const data = arr[1];
-//     const decodedData = JSON.parse(atob(data));
-//     return decodedData;
-//   } catch (e) {
-//     return "undefined";
-//   }
-// };
+const store3 = createStore(cartReducer);
 
-// function authReducer(state = {}, { type, token }) {
-//   if (type === "AUTH_LOGIN") {
-//     const payload = jwtDecode(token);
-//     return {
-//       token,
-//       payload,
-//     };
-//   } else if (type === "AUTH_LOGOUT") {
-//     return {};
-//   } else {
-//     return state;
-//   }
-// }
+store3.subscribe(() => console.log(store3.getState())); //
 
-// const actionAuthLogin = (token) => ({ type: "AUTH_LOGIN", token });
-// const actionAuthLogout = () => ({ type: "AUTH_LOGOUT" });
+console.log(store3.getState()); //{}
 
-// const token =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOiI2Mzc3ZTEzM2I3NGUxZjVmMmVjMWMxMjUiLCJsb2dpbiI6InRlc3Q1IiwiYWNsIjpbIjYzNzdlMTMzYjc0ZTFmNWYyZWMxYzEyNSIsInVzZXIiXX0sImlhdCI6MTY2ODgxMjQ1OH0.t1eQlRwkcP7v9JxUPMo3dcGKprH-uy8ujukNI7xE3A0";
+const actionCartAdd = (good, count = 1) => ({ type: "CART_ADD", count, good });
+const actionCartSub = (good, count = 1) => ({ type: "CART_SUB", count, good });
+const actionCartDel = (good) => ({ type: "CART_DEL", good });
+const actionCartSet = (good, count = 1) => ({ type: "CART_SET", count, good });
+const actionCartClear = () => ({ type: "CART_CLEAR" });
 
-// const store = createStore(authReducer);
-// store.subscribe(() => console.log(store.getState()));
+store3.dispatch(actionCartAdd({ _id: "пиво", price: 50 }));
+// {пиво: {good: {_id: 'пиво', price: 50}, count: 1}}
+store3.dispatch(actionCartAdd({ _id: "чіпси", price: 75 }));
+// {
+// пиво: {good: {_id: 'пиво', price: 50}, count: 1},
+// чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
+//}
+store3.dispatch(actionCartAdd({ _id: "пиво", price: 50 }, 5));
+// {
+// пиво: {good: {_id: 'пиво', price: 50}, count: 6},
+// чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
+//}
+store3.dispatch(actionCartSet({ _id: "чіпси", price: 75 }, 2));
+// {
+// пиво: {good: {_id: 'пиво', price: 50}, count: 6},
+// чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
+//}
 
-// store.dispatch(actionAuthLogin(token));
-// store.dispatch(actionAuthLogout());
+store3.dispatch(actionCartSub({ _id: "пиво", price: 50 }, 4));
+// {
+// пиво: {good: {_id: 'пиво', price: 50}, count: 2},
+// чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
+//}
 
+store3.dispatch(actionCartDel({ _id: "чіпси", price: 75 }));
+// {
+// пиво: {good: {_id: 'пиво', price: 50}, count: 2},
+//}
 
+store3.dispatch(actionCartClear()); // {}
 
-// //cartReducer
+//gql
 
-// function cartReducer(state = {}, { type, count, good }) {
-//   if (type === "CART_ADD") {
-//     const { _id } = good;
-//     if (state[_id]) {
-//       return {
-//         ...state,
-//         [_id]: { ...state[_id], count: state[_id].count + count },
-//       };
-//     } else {
-//       return {
-//         ...state,
-//         [_id]: { good, count },
-//       };
-//     }
-//   } else if (type === "CART_SUB") {
-//     const { _id } = good;
-//     let newCount = state[_id].count - count;
-//     if (newCount <= 0) {
-//       const newState = { ...state };
-//       delete newState[_id];
-//       return newState;
-//     } else {
-//       return {
-//         ...state,
-//         [_id]: { ...state[_id], count: newCount },
-//       };
-//     }
-//   } else if (type === "CART_DEL") {
-//     const { _id } = good;
-//     const newState = { ...state };
-//     delete newState[_id];
-//     return newState;
-//   } else if (type === "CART_SET") {
-//     const { _id } = good;
-//     if (count <= 0) {
-//       const newState = { ...state };
-//       delete newState[_id];
-//       return newState;
-//     } else {
-//       if (state[_id]) {
-//         return {
-//           ...state,
-//           [_id]: { ...state[_id], count },
-//         };
-//       } else {
-//         return {
-//           ...state,
-//           [_id]: { good, count },
-//         };
-//       }
-//     }
-//   } else if (type === "CART_CLEAR") {
-//     return {};
-//   } else {
-//     return state;
-//   }
-// }
+async function gql(query, variables) {
+  const url = "http://shop-roles.node.ed.asmer.org.ua/graphql";
+  const result = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ query: query, variables: variables }),
+  });
+  const data = await result.json();
+  return data;
+}
 
-// const store = createStore(cartReducer);
+if (localStorage.authToken) {
+  headers.Authorization = "Bearer " + localStorage.authToken;
+}
 
-// store.subscribe(() => console.log(store.getState())); //
+//Кореневі категорії
+const gqlRootCats = () => {
+  const catQuery = `query cats($q: String){
+    CategoryFind(query: $q){ 
+      _id name
+    }
+  }`;
+  const varRootCats = { q: "[{}]" };
+  return gql(catQuery, varRootCats);
+};
 
-// console.log(store.getState()); //{}
+const actionRootCats = () => actionPromise("rootCats", gqlRootCats());
 
-// const actionCartAdd = (good, count = 1) => ({ type: "CART_ADD", count, good });
-// const actionCartSub = (good, count = 1) => ({ type: "CART_SUB", count, good });
-// const actionCartDel = (good) => ({ type: "CART_DEL", good });
-// const actionCartSet = (good, count = 1) => ({ type: "CART_SET", count, good });
-// const actionCartClear = () => ({ type: "CART_CLEAR" });
+//Запит для отримання однієї категорії з товарами та картинками
+const gqlCatOne = (id) => {
+  const catOneQuery = `query oneCatWithGoodsImgs($q: String){
+    CategoryFindOne(query: $q){ _id name image{url}
+    goods{_id name price images{url}}
+  }}`;
+  return gql(catOneQuery, { q: JSON.stringify([{ _id: `${id}` }]) });
+};
 
-// store.dispatch(actionCartAdd({ _id: "пиво", price: 50 }));
-// // {пиво: {good: {_id: 'пиво', price: 50}, count: 1}}
-// store.dispatch(actionCartAdd({ _id: "чіпси", price: 75 }));
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 1},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
-// //}
-// store.dispatch(actionCartAdd({ _id: "пиво", price: 50 }, 5));
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 6},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
-// //}
-// store.dispatch(actionCartSet({ _id: "чіпси", price: 75 }, 2));
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 6},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
-// //}
+const actionCatOne = (id) => actionPromise("oneCat", gqlCatOne(id));
 
-// store.dispatch(actionCartSub({ _id: "пиво", price: 50 }, 4));
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 2},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
-// //}
+//Запит на отримання товару з описом та картинками
+const gqlGoodOne = (id) => {
+  const goodOneQuery = `query catsWithImgsDescription($q: String) {
+    GoodFindOne(query: $q) {
+      _id
+      images {
+        url
+      }
+      name
+      price
+      description
+    }
+  }`;
+  return gql(goodOneQuery, { q: JSON.stringify([{ _id: `${id}` }]) });
+};
 
-// store.dispatch(actionCartDel({ _id: "чіпси", price: 75 }));
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 2},
-// //}
+const actionGoodOne = (id) => actionPromise("oneGood", gqlGoodOne(id));
 
-// store.dispatch(actionCartClear()); // {}
+//Запит на реєстрацію
+const gqlRegister = (log, pass) => {
+  const registerQuery = `mutation reg($login: String!, $password: String!) {
+    UserUpsert(user: { login: $login, password: $password }) {
+      _id
+      login
+    }
+  }`;
+  return gql(registerQuery, { login: `${log}`, password: `${pass}` });
+};
+
+const actionRegister = (log, pass) =>
+  actionPromise("Registration", gqlRegister(log, pass));
+
+//Логін
+const actionLogin = (log, pass) => {
+  const gqlLogin = (log, pass) => {
+    const loginQuery = `query login($login:String, $password:String){
+      login(login:$login, password:$password)
+    }`;
+    return gql(loginQuery, { login: `${log}`, password: `${pass}` });
+  };
+  return actionPromise("Login", gqlLogin(log, pass));
+};
+
+//Запит історії замовлень
+const actionHistory = () => {
+  const gqlHistory = () => {
+    const historyQuery = `query OrderFind ($q: String) {OrderFind (query: $q) {
+      _id 
+      createdAt 
+      total
+    }}`;
+    return gql(historyQuery, { q: "[{}]" });
+  };
+  return actionPromise("History", gqlHistory());
+};
+
+//Запит оформлення замовлення
+const actionOrder = (count, id) => {
+  const gqlOrder = (count, id) => {
+    const orderQuery = `mutation newOrder($goods: [OrderGoodInput]) {
+      OrderUpsert(order: {orderGoods: $goods}) {
+        _id createdAt total
+      }
+    }`;
+    return gql(orderQuery, { goods: [{ count, good: { _id: `${id}` } }] });
+  };
+  return actionPromise("Order", gqlOrder(count, id));
+};
+
+// store.dispatch(actionRootCats());
+// store.dispatch(actionLogin("ryttyyy", "23232323"));
+// store.dispatch(actionRegister("ranastasy", "23232323"));
+// store.dispatch(actionCatOne("6262ca7dbf8b206433f5b3d1"));
+// store.dispatch(actionGoodOne("62d3099ab74e1f5f2ec1a125"));
+// store.dispatch(actionHistory());
+// store.dispatch(actionOrder(4, '62d3099ab74e1f5f2ec1a125'));
